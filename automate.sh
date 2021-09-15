37104a6b46ca897106640ca01eec2e59950a8d27
DIVYANSHU SHUKLA <86191568+justmorpheus@users.noreply.github.com>
2021-09-16 00:07:15 +0530

    Create automate.sh

---
+++ automate.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+apt update -y
+apt install apache2 -y 
+cd  /var/www/html/
+sudo wget https://github.com/justmorpheus/waf-dvwa/raw/main/DVWA.zip
+apt install unzip -y
+unzip dvwa.zip
+apt install mysql-server -y
+apt install php -y 
+apt install php-mysqli -y
+service apache2 restart
+
+mysql -u root -p123 <<EOF
+CREATE USER 'demo'@'%' IDENTIFIED BY 'password@123#';
+GRANT ALL PRIVILEGES ON *.* TO 'demo'@'%';
+flush privileges;
+EOF
